fcff9cda14a58b9f7596afb7ce42da8b043a60ce
Nguyen Dang Phat <nguyendangphat1505@gmail.com>
2025-03-26 15:12:03 +0700

    UP

--- backend/TuyenDungAPI/Models/Product.cs
+++
@@ -1,9 +0,0 @@
-namespace TuyenDungAPI.Models
-{
-    public class Product
-    {
-        public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
-        public double Price { get; set; }
-    }
-}
