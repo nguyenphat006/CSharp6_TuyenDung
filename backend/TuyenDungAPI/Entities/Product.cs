1430ceebf949ef441d08cbde9d5e1aa0f42d3530
Nguyen Dang Phat <nguyendangphat1505@gmail.com>
2025-03-26 19:02:24 +0700

    up

---
+++ backend/TuyenDungAPI/Entities/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace TuyenDungAPI.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Description { get; set; } = string.Empty;
+        public required string FirstName { get; set; } = string.Empty;
+
+    }
+}
---
+++ backend/TuyenDungAPI/Entities/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TuyenDungAPI.Entities
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+    }
+}
