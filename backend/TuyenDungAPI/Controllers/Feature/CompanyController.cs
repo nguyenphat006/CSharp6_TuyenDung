3028bc7cc3c77ed891206f483304fda0ad84e7ed
Nguyen Dang Phat <nguyendangphat1505@gmail.com>
2025-04-16 14:26:17 +0700

    .

--- backend/TuyenDungAPI/Controllers/Feature/CompanyController.cs
+++ backend/TuyenDungAPI/Controllers/Feature/CompanyController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 using TuyenDungAPI.Model.Company;
@@ -9,6 +10,7 @@ namespace TuyenDungAPI.Controllers.Feature
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class CompanyController : ControllerBase
     {
         private readonly CompanyService _companyService;
